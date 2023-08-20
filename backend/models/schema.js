128bf13e53971e4b9f2c24b754692c4a51c10d56
Mainak <dmainak706@gmail.com>
2023-08-20 16:49:41 +0530

    second commit

--- backend/models/schema.js
+++ backend/models/schema.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // import mongoose from "mongoose";
-const { Schema, model } = mongoose;
+const { Schema } = mongoose;
 
 const promptSchema = new Schema({
 	prompt: {type: String, required: true}
